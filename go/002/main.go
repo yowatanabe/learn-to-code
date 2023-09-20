b7b71837cf2ec44a09fd22d83353c52ce204adf5
yowatanabe <57588645+yowatanabe@users.noreply.github.com>
2023-09-21 06:51:07 +0900

    Add go/002/

---
+++ go/002/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var temperature float64 = 26.5
+    var isSunny bool = true
+
+    fmt.Printf("Temperature: %.1f\n", temperature)
+    fmt.Printf("Is it sunny today? %v\n", isSunny)
+}
