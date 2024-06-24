77ede86b3a233a984934b6684c985d8308e73e03
yowatanabe <57588645+yowatanabe@users.noreply.github.com>
2024-06-25 05:29:51 +0900

    Add sql/068/

---
+++ sql/068/answer.sql
@@ -0,0 +1,5 @@
+-- EmployeesテーブルのHireDate列にインデックスを作成
+CREATE INDEX idx_hire_date ON Employees (HireDate);
+
+-- EmployeesテーブルのDepartmentID列にインデックスを作成
+CREATE INDEX idx_department_id ON Employees (DepartmentID);
