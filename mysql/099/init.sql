3e115a524593b574467cf2e29bb3cfd19de04270
yowatanabe <57588645+yowatanabe@users.noreply.github.com>
2024-11-03 05:21:33 +0900

    Add mysql/099/

---
+++ mysql/099/init.sql
@@ -0,0 +1,23 @@
+-- データベースの作成
+DROP DATABASE IF EXISTS learn_to_code;
+
+CREATE DATABASE learn_to_code;
+
+-- データベースの選択
+USE learn_to_code;
+
+-- テーブルの作成
+CREATE TABLE
+  Accounts (
+    AccountID INT PRIMARY KEY,
+    CustomerName VARCHAR(100),
+    Balance DECIMAL(10, 2) CHECK (Balance >= 0)
+  );
+
+-- 初期データ挿入
+INSERT INTO
+  Accounts (AccountID, CustomerName, Balance)
+VALUES
+  (1, 'Alice', 1000),
+  (2, 'Bob', 3000),
+  (3, 'Charlie', 7000);
