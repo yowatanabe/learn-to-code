0d85b5ea040dd139212af126a1d8c5a484019f3a
yowatanabe <57588645+yowatanabe@users.noreply.github.com>
2025-06-23 04:41:41 +0900

    Add python/131/

---
+++ python/131/main.py
@@ -0,0 +1,8 @@
+def reverse_string(s):
+    return s[::-1]
+
+
+# テスト
+print(reverse_string("hello"))  # 出力: "olleh"
+print(reverse_string("Python"))  # 出力: "nohtyP"
+print(reverse_string(""))  # 出力: ""
