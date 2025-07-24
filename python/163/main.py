97df5766c2f634ca628f56fb78486e1fd9f90b80
yowatanabe <57588645+yowatanabe@users.noreply.github.com>
2025-07-25 04:53:55 +0900

    Add python/163/

---
+++ python/163/main.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if stack and stack[-1] == char:
+                stack.pop()
+            else:
+                stack.append(char)
+        return "".join(stack)
+
+
+# テスト
+sol = Solution()
+print(sol.removeDuplicates("abbaca"))  # 出力: "ca"
+print(sol.removeDuplicates("azxxzy"))  # 出力: "ay"
+print(sol.removeDuplicates("aabbcc"))  # 出力: ""
+print(sol.removeDuplicates("abcddcba"))  # 出力: ""
